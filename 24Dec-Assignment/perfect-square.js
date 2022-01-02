4a08f76fcd8751d1bec4835216fdc76891805c61
Shivani Kashyap <79568372+shivanikashyap27@users.noreply.github.com>
2022-01-02 16:17:17 +0530

    Add files via upload

---
+++ 24Dec-Assignment/perfect-square.js
@@ -0,0 +1,8 @@
+var n=prompt("Enter a number ");
+n=parseInt(n);
+
+if(Math.sqrt(n)%1===0){
+    alert( n+" is Perfect Square.");
+}else{
+    alert( n+ " is Not Perfect Square.");
+}
---
+++ 24Dec-Assignment/prime.js
@@ -0,0 +1,21 @@
+const n = parseInt(prompt('Enter higher number: '));
+
+console.log('The prime numbers of ' + n + ' are:');
+
+// looping from lowerNumber to higherNumber
+for (let i = 1; i <= n; i++) {
+    let flag = 0;
+
+    // looping through 2 to user input number
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    // if number greater than 1 and not divisible by other numbers
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
+}
