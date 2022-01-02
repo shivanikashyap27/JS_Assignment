d2efee21525c678587688bd4ecee96b6979c2784
Shivani Kashyap <79568372+shivanikashyap27@users.noreply.github.com>
2022-01-02 15:01:52 +0530

    Add files via upload

---
+++ 20DEC-Assignment"/max.js
@@ -0,0 +1,17 @@
+var num1 = 3; 
+ var num2 = 9; 
+ var num3 = 7; 
+var largest;
+
+// check the condition
+if(num1 >= num2 && num1 >= num3) {
+    largest = num1;
+}
+else if (num2 >= num1 && num2 >= num3) {
+    largest = num2;
+}
+else {
+    largest = num3;
+}
+
+console.log("The largest number is " + largest);  
