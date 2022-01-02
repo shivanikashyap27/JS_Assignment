a2347c6636d8197099b31a6e65cd3dbdbe76c962
Shivani Kashyap <79568372+shivanikashyap27@users.noreply.github.com>
2022-01-02 15:41:57 +0530

    Add files via upload

---
+++ 21Dec-Assignment"/smallest.js
@@ -0,0 +1,7 @@
+var a = prompt("Enter first number: ");
+var b = prompt("Enter second number: ");
+var c = prompt("Enter third number: ");
+
+console.log(a <= b);
+console.log(b <= c);
+console.log(c <= a);
